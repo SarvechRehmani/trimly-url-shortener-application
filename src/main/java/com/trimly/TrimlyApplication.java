1ea522d6c59836189456048a2095b6f7b294fd82
Sarvech <104658538+SarvechRehmani@users.noreply.github.com>
2025-07-15 17:26:05 +0500

    ..

--- src/main/java/com/trimly/TrimlyApplication.java
+++ src/main/java/com/trimly/TrimlyApplication.java
@@ -8,7 +8,7 @@ public class TrimlyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrimlyApplication.class, args);
-		System.out.println("---PROJECT STARTED---");
+		System.out.println("---TRIMLY PROJECT STARTED---");
 	}
 
 }
